a765791aad28c810e496a23fec12d1ad9a031969
Nachoo1899 <ignaciopintosdsilva@gmail.com>
2025-06-17 18:48:53 -0300

    Delete token.js

--- token.js
+++
@@ -1,9 +0,0 @@
-
-// Validación de token para activar el chat (sin bloquear la página entera)
-const urlParams = new URLSearchParams(window.location.search);
-const token = urlParams.get("token");
-const validTokens = ["abc123", "def456", "ghi789"];
-
-if (token && validTokens.includes(token)) {
-  document.getElementById("chat-container").classList.remove("hidden");
-}
